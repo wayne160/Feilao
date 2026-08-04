6deb8beb3fbc3a70eb7f3f4cec03b102e66280c4
Weicheng Zhang <wz26099@gmail.com>
2026-08-04 17:56:11 +1000

    print test done

--- backend/app/routers/print.py
+++ backend/app/routers/print.py
@@ -1,33 +1,33 @@
-from escpos.printer import Win32Raw
-from fastapi import APIRouter
-import win32print
+# from escpos.printer import Win32Raw
+# from fastapi import APIRouter
+# import win32print
 
-router = APIRouter(tags=["print"])
+# router = APIRouter(tags=["print"])
 
-PRINTER_NAME = "Generic"  # must match the name in Windows > Printers & scanners
+# PRINTER_NAME = "Generic"  # must match the name in Windows > Printers & scanners
 
-@router.get("/print")
-def print_test() -> dict[str, str]:
-    data = b"\x1b\x40"        # initialize printer
-    data += b"TEST ORDER\n"   # text
-    data += b"TEST ORDER\n"
-    data += b"TEST ORDER\n"
-    data += b"TEST ORDER\n"
-    data += b"TEST ORDER\n"
-    data += b"TEST ORDER\n"
-    data += b"\x1d\x56\x00"
-    hPrinter = win32print.OpenPrinter('Generic')
-    try:
-        hJob = win32print.StartDocPrinter(hPrinter, 1, ("Order", None, "RAW"))
-        try:
-            win32print.StartPagePrinter(hPrinter)
-            win32print.WritePrinter(hPrinter, data)
+# @router.get("/print")
+# def print_test() -> dict[str, str]:
+#     data = b"\x1b\x40"        # initialize printer
+#     data += b"TEST ORDER\n"   # text
+#     data += b"TEST ORDER\n"
+#     data += b"TEST ORDER\n"
+#     data += b"TEST ORDER\n"
+#     data += b"TEST ORDER\n"
+#     data += b"TEST ORDER\n"
+#     data += b"\x1d\x56\x00"
+#     hPrinter = win32print.OpenPrinter('Generic')
+#     try:
+#         hJob = win32print.StartDocPrinter(hPrinter, 1, ("Order", None, "RAW"))
+#         try:
+#             win32print.StartPagePrinter(hPrinter)
+#             win32print.WritePrinter(hPrinter, data)
 
-            win32print.EndPagePrinter(hPrinter)
-        finally:
-            win32print.EndDocPrinter(hPrinter)
-    finally:
-        win32print.ClosePrinter(hPrinter)
+#             win32print.EndPagePrinter(hPrinter)
+#         finally:
+#             win32print.EndDocPrinter(hPrinter)
+#     finally:
+#         win32print.ClosePrinter(hPrinter)
 
-    return {"status": "ok"}
+#     return {"status": "ok"}
 
