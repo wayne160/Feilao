e73a82e909b5110b1b56651857197e387a22db8a
Weicheng Zhang <wz26099@gmail.com>
2026-08-04 20:17:01 +1000

    hello test

--- backend/app/main.py
+++ backend/app/main.py
@@ -25,7 +25,7 @@ print("cors is " + os.getenv("CORS_ORIGINS"))
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[os.getenv("CORS_ORIGINS")]
+    allow_origins=[os.getenv("CORS_ORIGINS")],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
